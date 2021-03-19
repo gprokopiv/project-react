ff4283da92df76b167cd2531d1374ffa0e63e591
Prokopiv <halyna.kotlyar@gmail.com>
2021-03-19 15:56:10 +0200

    react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Container from "./components/UI/Container/index";
+import './App.css';
+
+function App() {
+  return (
+    <Container>
+
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/UI/Container/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Container = (() => {
+    return (
+        <div>
+            
+        </div>
+    );
+});
+
+export default Container;
