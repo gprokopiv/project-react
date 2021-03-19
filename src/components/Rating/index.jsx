fd9e4985ad10aba7eb97d107165ce101f7120c2a
Prokopiv <halyna.kotlyar@gmail.com>
2021-03-20 00:36:01 +0200

    rating

---
+++ src/components/Rating/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import s from './rating.module.css'
+
+const Rating = props => {
+    return (
+        <div className={s.rating}>
+            
+        </div>
+    );
+};
+
+Rating.propTypes = {
+    
+};
+
+export default Rating;
