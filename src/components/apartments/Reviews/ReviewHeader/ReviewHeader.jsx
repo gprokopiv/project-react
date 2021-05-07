8277271c530b3fdedb011c2619fab24f7804cc96
Prokopiv <halyna.kotlyar@gmail.com>
2021-05-07 12:04:48 +0300

    reviewHeader

--- src/components/apartments/Reviews/ReviewHeader/ReviewHeader.jsx
+++ src/components/apartments/Reviews/ReviewHeader/ReviewHeader.jsx
@@ -1,16 +1,23 @@
 import PropTypes from 'prop-types';
+import Rating from '../../../Rating';
+import s from './ReviewHeader.module.css';
 
-const ReviewHeader = props => {
+const ReviewHeader = ({ totalRating, amount }) => {
 return (
-  <div>
-
+  <div className={s.content}>
+    <h2 className={s.title}> Total rating</h2>
+    <div className={s.rating}>
+     <span>{amount} reviews</span>
+     <Rating rating ={totalRating}/>
+    </div>
   </div>
 );
 
 };
 
 ReviewHeader.propTypes = {
-
+  totalRating: PropTypes.number.isRequired,
+  amount: PropTypes.number.isRequired,
 };
 
 export default ReviewHeader;
